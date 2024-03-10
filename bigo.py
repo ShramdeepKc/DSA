c2ac8521de9cd4b88861bf5fa3ecf0b6fc9a66b5
Shramdeep Kc <shramdeepkc21@gmail.com>
2024-03-10 13:36:29 +0545

    linkedlist

--- bigo.py
+++ bigo.py
@@ -1,3 +1,4 @@
+# BASICS OF PYTHON
 def find_max(lst):
     max_num = float('-inf')  # Initialize max_num to negative infinity
     for num in lst:
@@ -18,8 +19,10 @@ print(stock_prices[3])
 class Cookie:
     def __init__(self, color):
         self.color = color
+
     def get_color(self):
         return self.color
+
     def set_color(self):
         return self.color
 
@@ -30,10 +33,74 @@ cookie_2 = Cookie('#00FF00')
 print('cookie is', cookie_1.get_color())
 print('cookie is', cookie_2.get_color())
 
-
 num4 = 10
 num3 = num4
-print ("num", num4)
-print ("num", num3)
-print ("num", id(num4))
-print ("num", id(num3))
+print("num", num4)
+print("num", num3)
+print("num", id(num4))
+print("num", id(num3))
+
+
+# DATASTRUCTURES AND ALGORITHMS LINKED LIST
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+my_linked_list.pop()
+my_linked_list.pop()
+my_linked_list.pop()
+my_linked_list.pop()
+
+print(my_linked_list.pop())
+
+my_linked_list.print_list()
